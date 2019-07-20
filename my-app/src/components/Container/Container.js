3b5059927a2741dac2b89d818afc897989bd6baf
toobaditstaken <alexabbott10@yahoo.com>
2019-07-20 10:58:36 -0500

    cleanup complete

--- my-app/src/components/Container/Container.js
+++ my-app/src/components/Container/Container.js
@@ -2,12 +2,7 @@ import React from 'react';
 import './Container.css';
 import Character from '../Character';
 
-// main container for each Character component
-// loops through each index in props.characters, which contains an array of character images
-// to create a new Character component for each image
-// attaches the passed down clickEvent function to each Character component
 const Container = props => (
-  // loops through
   <div
     className={
       props.shake
