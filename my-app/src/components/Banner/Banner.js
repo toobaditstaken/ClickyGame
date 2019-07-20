e8416dae5524deedebccf4cdac4586a60db1e84a
toobaditstaken <alexabbott10@yahoo.com>
2019-07-20 10:54:44 -0500

    fully coded beggining cleanup

---
+++ my-app/src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">Click on an image to earn points, but don't click the same one!</p>
+  </div>
+);
+
+export default Banner;
---
+++ my-app/src/images.js
@@ -0,0 +1,29 @@
+import anri from './images/Anri.jpg';
+import black from './images/Black.png';
+import eygon from './images/Eygon.jpg';
+import gael from './images/Gael.png';
+import giant from './images/Giant.png';
+import handmaiden from './images/Handmaiden.jpg';
+import havel from './images/Havel.png';
+import hawkwood from './images/Hawkwood.jpg';
+import horace from './images/Horace.jpg';
+import irina from './images/Irina.jpg';
+import onion from './images/Onion.png';
+import patches from './images/Patches.jpg';
+
+const images = [
+  anri,
+  black,
+  eygon,
+  gael,
+  giant,
+  handmaiden,
+  havel,
+  hawkwood,
+  horace,
+  irina,
+  onion,
+  patches
+];
+
+export default images;
